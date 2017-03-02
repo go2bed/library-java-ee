455cf6b8f854de3c6afc7c59520337115f361f27
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-03-02 17:55:27 +0600

    [FIX] Fixed errors during mvn build [IMP] Implemented new common module for the web services

--- java-library-ee-web/src/main/java/com/epam/chadov/web/services/LibraryJaxWsService.java
+++ java-library-ee-web/src/main/java/com/epam/chadov/web/services/LibraryJaxWsService.java
@@ -1,7 +1,22 @@
 package com.epam.chadov.web.services;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 /**
  * @author Andrey_Chadov on 3/2/2017.
  */
+
+@WebService
 public class LibraryJaxWsService {
+
+    private final String message = "Hello, ";
+
+    public LibraryJaxWsService() {
+    }
+
+    @WebMethod
+    public String sayHello(String name) {
+        return message + name + ".";
+    }
 }
